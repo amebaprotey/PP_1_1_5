e23ca3183621e7b428f83b327a73760b939230c1
hrulev <amebaprotey@rambler.ru>
2023-04-12 23:45:59 +0300

    commit4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.SessionFactory;
@@ -13,7 +14,7 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ivan", "Ivanov", (byte) 26);
         System.out.println("User c именем " + userService.getAllUsers().get(userService.getAllUsers().size() - 1).getName() + " добавлен в базу данных");
